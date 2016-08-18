1468daa6ee6d7ff5ebb024062766bae995781412
Rick van den Hof <r.hof@avisi.nl>
2016-08-18 16:57:28 +0200

    Add compose command
    Added recompose ability to VAPP class.
    Added clone ability to VCA class.
    
    Bump version number
    
    Add connect_vm and disconnect_vm methods
    
    Add support for choosing a Storage Profile
    
    PEP8 fixes
    
    Moved recompose to vcloudair class and apply PEP8 style fixes
    
    Check for del_vm_specs
    
    Move params code to generic method
    
    Fix modify vm methods
    
    Fix links
    
    Fix links
    
    Remove code for setting cpus/memory during (re)compose, it is not supported
    
    Prevent exception when not passing add_vm_specs
    
    Add ability to specify targetVM for deploy and undeploy actions
    
    Add ability to specify targetVM for deploy and undeploy actions
    
    Return proper exception messages
    
    Add disk usage to vm details results and ability to add a disk to a running vm
    
    Specify storage profile for extra disks

--- tests/vca_tests.py
+++ tests/vca_tests.py
@@ -6,11 +6,9 @@ from pyvcloud.vcloudair import VCA
 
 class TestVCA:
 
-
     def __init__(self):
         self.vca = None
         self.login_to_vcloud()
-
 
     def login_to_vcloud(self):
         """Login to vCloud"""
@@ -19,7 +17,13 @@ class TestVCA:
         service_type = config['vcloud']['service_type']
         host = config['vcloud']['host']
         version = config['vcloud']['version']
-        self.vca = VCA(host=host, username=username, service_type=service_type, version=version, verify=True, log=True)
+        self.vca = VCA(
+            host=host,
+            username=username,
+            service_type=service_type,
+            version=version,
+            verify=True,
+            log=True)
         assert self.vca
         if vcloudair.VCA_SERVICE_TYPE_STANDALONE == service_type:
             raise Exception('not-supported')
@@ -28,7 +32,6 @@ class TestVCA:
         elif vcloudair.VCA_SERVICE_TYPE_ONDEMAND == service_type:
             result = self.vca.login(password=password)
             assert result
-
 
     def logout_from_vcloud(self):
         """Logout from vCloud"""
@@ -41,30 +44,26 @@ class TestVCA:
         """Loggin in to vCloud"""
         assert self.vca.token
 
-
     def test_0002(self):
         """Get Service Groups"""
         service_groups = self.vca.get_service_groups()
         assert service_groups
-        assert len(service_groups)>=0
+        assert len(service_groups) >= 0
 
     def test_0003(self):
         """Get Plans"""
         plans = self.vca.get_plans()
         assert plans
-        assert len(plans)>=0
+        assert len(plans) >= 0
 
     def test_0004(self):
         """Get Instances"""
         instances = self.vca.get_instances()
         assert instances
-        assert len(instances)>=0
-
+        assert len(instances) >= 0
 
     def test_0005(self):
         """Get Users"""
         users = self.vca.get_users()
         assert users
-        assert len(users)>=0
-
-
+        assert len(users) >= 0
